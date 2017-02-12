4e743e76287be8c708ba6bdd76dec1a6e86e05ad
mvfe8080 <mvfe8080@gmail.com>
2017-02-13 01:56:07 +0900

    layout completed

--- src/home/Test.vue
+++ src/home/Test.vue
@@ -13,3 +13,26 @@ export default {
 <style scoped>
 
 </style>
+
+
+<md-card>
+  <md-card-area>
+    <md-card-media>
+      <img src="../assets/card-image-1.jpg" alt="People">
+    </md-card-media>
+
+    <md-card-header>
+      <div class="md-title">Title goes here</div>
+      <div class="md-subhead">Subtitle here</div>
+    </md-card-header>
+
+    <md-card-content>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+    </md-card-content>
+  </md-card-area>
+
+  <md-card-actions>
+    <md-button>Action</md-button>
+    <md-button>Action</md-button>
+  </md-card-actions>
+</md-card>
